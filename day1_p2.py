7f1c065a7357e89882eb56ed6acf97c042acdf05
ltj623 <ltj623@vt.edu>
2024-12-13 12:15:47 -0500

    no need to sort

--- day1_p2.py
+++ day1_p2.py
@@ -7,16 +7,12 @@ with open("/Users/ltj623/Desktop/input.txt", "r") as file:
         list1.append(int(numbers[0]))
         list2.append(int(numbers[1]))
 
-list1.sort()
-list2.sort()
-
 total = 0
 h = {}
 
 for i, num in enumerate(list1):
-    print(i, num)
     if num not in h:
         h[num] = num * list2.count(num)
     total += h[num]
-    
+
 print(total)
